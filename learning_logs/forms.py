0342d2c4ece09235d63e5feebd26fc7d54e0816e
Timofey Kuzmin <sinnerpocht@yandex.ru>
2017-07-02 22:03:52 +0300

    Ready for deployment to heroku.

---
+++ learning_logs/forms.py
@@ -0,0 +1,17 @@
+from  .models import Topic, Entry
+
+from django import forms
+
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model =  Topic
+        fields = ['text']
+        labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80, 'style': 'border:1px solid #eee; resize:none'})}
